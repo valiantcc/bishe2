606e9de77f884a8e9f6c20fcfd7c352ec55dd60d
valiantcc <92305424+valiantcc@users.noreply.github.com>
2025-04-09 11:17:31 +0800

    Add files via upload

---
+++ rag_flutter_app/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Devs\\Flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Devs\\rag_flutter_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Devs\\Flutter"
+  "PROJECT_DIR=D:\\Devs\\rag_flutter_app"
+  "FLUTTER_ROOT=D:\\Devs\\Flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Devs\\rag_flutter_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Devs\\rag_flutter_app"
+  "FLUTTER_TARGET=D:\\Devs\\rag_flutter_app\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Devs\\rag_flutter_app\\.dart_tool\\package_config.json"
+)
